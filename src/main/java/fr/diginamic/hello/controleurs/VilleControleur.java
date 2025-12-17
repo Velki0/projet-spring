aee0cea69ef5d4ceb2184a0af23ff844ccd3425b
Velki0 <b.gerardin125@gmail.com>
2025-12-17 16:43:26 +0100

    Réalisation TP04 et 05

--- src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
@@ -1,21 +1,31 @@
 package fr.diginamic.hello.controleurs;
 
 import fr.diginamic.hello.entites.Ville;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 @RestController
-@RequestMapping("/villes")
+@RequestMapping(path = "/villes")
 public class VilleControleur {
 
-    @GetMapping
-    public List<Ville> getVilles() {
+    private static final Set<Ville> listeVilles = new HashSet<>();
 
-        List<Ville> listeVilles = new ArrayList<>();
+    @PostConstruct
+    public void initData() {
+
         listeVilles.add(new Ville("Nice", 343_000));
         listeVilles.add(new Ville("Carcassonne", 47_800));
         listeVilles.add(new Ville("Narbonne", 53_400));
@@ -24,7 +34,65 @@ public class VilleControleur {
         listeVilles.add(new Ville("Pau", 77_200));
         listeVilles.add(new Ville("Marseille", 850_700));
         listeVilles.add(new Ville("Tarbes", 40_600));
-        return listeVilles;
+
+    }
+
+    @GetMapping
+    public ResponseEntity<Set<Ville>> getVilles() {
+
+        return ResponseEntity.ok(listeVilles);
+
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<String> getVilleById(@PathVariable int id) {
+
+        Optional<Ville> villeEnBase = listeVilles.stream().filter(v -> v.getId() == id).findFirst();
+        if (villeEnBase.isPresent()) {
+            return ResponseEntity.ok("Voici la ville demandée : " + villeEnBase.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("La ville avec l'id : " + id + " n'est pas présente.");
+        }
+
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addVille(@RequestBody Ville ville) {
+
+        Optional<Ville> villeEnBase = listeVilles.stream().filter(v -> v.getNom().equals(ville.getNom())).findFirst();
+        if(villeEnBase.isEmpty()) {
+            listeVilles.add(ville);
+            return ResponseEntity.status(HttpStatus.CREATED).body("La ville de : " + ville.getNom() + " a été créée avec succès.");
+        } else {
+            return ResponseEntity.badRequest().body("La ville de : " + ville.getNom() + " existe déjà.");
+        }
+
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateVille(@RequestBody Ville ville) {
+
+        Optional<Ville> villeEnBase = listeVilles.stream().filter(v -> v.getNom().equals(ville.getNom())).findFirst();
+        if(villeEnBase.isPresent()) {
+            villeEnBase.get().setNom(ville.getNom());
+            villeEnBase.get().setPopulation(ville.getPopulation());
+            return ResponseEntity.ok("La ville de " + ville.getNom() + " a bien été modifiée.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("La ville de : " + ville.getNom() + " n'est pas présente. Impossible de la modifier.");
+        }
+
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<String> deleteVille(@PathVariable int id) {
+
+        Optional<Ville> villeEnBase = listeVilles.stream().filter(v -> v.getId() == id).findFirst();
+        if(villeEnBase.isPresent()) {
+            listeVilles.remove(villeEnBase.get());
+            return ResponseEntity.ok("La ville de " + villeEnBase.get().getNom() + " a bien été supprimée.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("La ville avec l'id : " + id + " n'est pas présente. Impossible de la supprimer.");
+        }
 
     }
 
--- src/main/java/fr/diginamic/hello/entites/Ville.java
+++ src/main/java/fr/diginamic/hello/entites/Ville.java
@@ -2,20 +2,32 @@ package fr.diginamic.hello.entites;
 
 public class Ville {
 
+    private static int numId = 1;
+
+    private final int id;
     private String nom;
     private int population;
 
     public Ville(String nom, int population) {
 
+        this.id = numId++;
         this.nom = nom;
         this.population = population;
 
     }
 
+    public int getId() { return id; }
     public String getNom() { return nom; }
     public int getPopulation() { return population; }
 
     public void setNom(String nom) { this.nom = nom; }
     public void setPopulation(int population) { this.population = population; }
+
+    @Override
+    public String toString() {
+
+        return "{" +id + ", " + nom + ", " + population + "}";
+
+    }
 
 }
