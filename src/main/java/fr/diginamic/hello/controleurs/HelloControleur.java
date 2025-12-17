ecc16a5f74bd471134a69b935baca4ace573a8ee
Velki0 <b.gerardin125@gmail.com>
2025-12-17 12:05:54 +0100

    Réalisation TP02

--- src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
@@ -1,5 +1,7 @@
 package fr.diginamic.hello.controleurs;
 
+import fr.diginamic.hello.services.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloControleur {
 
+    @Autowired
+    private HelloService helloService;
+
     @GetMapping
     public String direHello(){
-        return "Hello";
+        return helloService.envoyerSalutation();
     }
 
 }
---
+++ src/main/java/fr/diginamic/hello/services/HelloService.java
@@ -0,0 +1,12 @@
+package fr.diginamic.hello.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String envoyerSalutation(){
+        return "Je suis la classe de service et je vous dis Bonjour!";
+    }
+
+}
