14634b7f37630acb60db62369b28d61678510fa8
Velki0 <b.gerardin125@gmail.com>
2025-12-17 10:20:35 +0100

    Réalisation TP01

---
+++ src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
@@ -0,0 +1,16 @@
+package fr.diginamic.hello.controleurs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloControleur {
+
+    @GetMapping
+    public String direHello(){
+        return "Hello";
+    }
+
+}
