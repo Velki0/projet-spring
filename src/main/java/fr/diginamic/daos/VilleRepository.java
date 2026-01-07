7a072a86dc3cbec33751488000e27b78776f6fb2
Velki0 <b.gerardin125@gmail.com>
2026-01-07 10:40:31 +0100

    Correctif mineur

--- src/main/java/fr/diginamic/daos/VilleRepository.java
+++ src/main/java/fr/diginamic/daos/VilleRepository.java
@@ -22,15 +22,15 @@ public interface VilleRepository extends JpaRepository<Ville,Integer> {
     List<Ville> findByPopulationBetweenOrderByPopulationDesc(int min, int max);
 
     // Recherche de toutes les villes d’un département dont la population est supérieure à min. Les villes sont retournées par population descendante.
-    @Query("SELECT DISTINCT v FROM Ville v WHERE v.departement.codeDepartement = :codeDptm AND v.population >= :min ORDER BY v.population DESC")
+    @Query("SELECT v FROM Ville v WHERE v.departement.codeDepartement = :codeDptm AND v.population >= :min ORDER BY v.population DESC")
     List<Ville> findAllVilleFromDepartementPopulationMin(String codeDptm, int min);
 
     // Recherche de toutes les villes d’un département dont la population est supérieure à min et inférieure à max. Les villes sont retournées par population descendante.
-    @Query("SELECT DISTINCT v FROM Ville v WHERE v.departement.codeDepartement = :codeDptm AND v.population BETWEEN :min AND :max ORDER BY v.population DESC")
+    @Query("SELECT v FROM Ville v WHERE v.departement.codeDepartement = :codeDptm AND v.population BETWEEN :min AND :max ORDER BY v.population DESC")
     List<Ville> findAllVilleFromDepartementPopulationBetween(String codeDptm, int min, int max);
 
     // Recherche des n villes les plus peuplées d’un département donné.
-    @Query("SELECT DISTINCT v FROM Ville v WHERE v.departement.codeDepartement = :codeDptm ORDER BY v.population DESC LIMIT :top")
+    @Query("SELECT v FROM Ville v WHERE v.departement.codeDepartement = :codeDptm ORDER BY v.population DESC LIMIT :top")
     List<Ville> findTopVilleFromDepartement(int top, String codeDptm);
 
 }
