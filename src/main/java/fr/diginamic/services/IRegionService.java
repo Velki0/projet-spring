ed21c2377cd373a188587be4ec05b2ce0d5da423
Velki0 <b.gerardin125@gmail.com>
2026-01-08 14:21:41 +0100

    Correctif mineur

--- src/main/java/fr/diginamic/services/IRegionService.java
+++ src/main/java/fr/diginamic/services/IRegionService.java
@@ -4,5 +4,7 @@ import fr.diginamic.entites.Region;
 import org.springframework.data.domain.Page;
 
 public interface IRegionService {
+
     Page<Region> getAllRegions(Integer page, Integer taille);
+
 }
